44529608312b9e87156a2d422848ec42b0f77cd9
n0rb1v <n0rb1v@gmail.com>
2021-04-08 15:35:30 +0200

    2

--- src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
@@ -1,4 +1,24 @@
 package hu.nive.ujratervezes.zarovizsga.people;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class People {
+    public int getNumberOfMales(String s) {
+        try (BufferedReader br = Files.newBufferedReader(Path.of(s))) {
+            String line = br.readLine();
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                String[] tmp = line.split(",");
+                if (tmp[4].equals("Male")) {
+                    sum++;
+                }
+            }
+            return sum;
+        } catch (IOException e) {
+            throw new IllegalStateException("file error", e);
+        }
+    }
 }
